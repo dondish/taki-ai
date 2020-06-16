76a196adc621682bf96bbd7510fa90511efb4040
dondish <odedshapira1@gmail.com>
2020-06-16 18:57:02 +0300

    let's you play against more advanced AI.

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from agents.dqn import AIAgent
 
 if __name__ == '__main__':
     # Normal game, 4 players, 3 DQN agents and 1 human
-    game = Game([AIAgent('./models/checkpoint1592310334.794725'), AIAgent('./models/checkpoint1592310334.794725'),
-                 AIAgent('./models/checkpoint1592310334.794725'), HumanAgent()], True)
+    game = Game([AIAgent('./models/checkpoint1592317337.915795'), AIAgent('./models/checkpoint1592317337.915795'),
+                 AIAgent('./models/checkpoint1592317337.915795'), HumanAgent()], True)
     # print(game.deck)
     print(game.observation().shape)
     while not game.done():
